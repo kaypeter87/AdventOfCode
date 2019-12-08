71f2aca8a6395e4cf707c55456cacc2fbfc16cb6
Peter Kay <contact@kaypeter.com>
2019-12-08 16:56:51 -0500

    add integer into array

--- Day2/day2.go
+++ Day2/day2.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var perline int
+	var nums []int
+
+	for {
+		_, err := fmt.Fscanf(file, "%d", &perline)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		nums = append(nums, perline)
+	}
+
+	fmt.Println(nums)
 }
